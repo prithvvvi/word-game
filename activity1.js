1d5d49db944a078ff329f3d9a74e052235cd5266
prithvvvi <92890640+prithvvvi@users.noreply.github.com>
2021-12-07 16:44:31 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,12 @@
+var score=0;
+function update_score(){
+    score=score+1;
+    document.getElementById("score").innerHTML="SCORE: "+score;
+
+}
+function save_score(){
+    localStorage.setItem("score",score);
+}
+function next_page(){
+    window.location="activity_2.html";
+}
